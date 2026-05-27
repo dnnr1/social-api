194704c8bc54e4d35672bf09f9e43172d49e038c
Daniel Roberto <sdanirobert@outlook.com>
2026-05-26 22:45:10 -0300

    feat: add helmet

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import route from "./routes/router.js";
 import errorHandler from "./utils/errorHandler.js";
+import helmet from "helmet";
 
 const app = express();
 
@@ -12,6 +13,7 @@ const corsConfig = {
 
 const port = 8080;
 
+app.use(helmet());
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
