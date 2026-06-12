ff7a387b212f7b6c504a9ab2bdb7cfe4ecb3df06
Daniel Roberto <sdanirobert@outlook.com>
2026-06-12 05:16:54 -0300

    fix: use AppError in auth middleware instead of generic Error

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,21 +1,22 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { TokenPayload } from "../types/types.js";
-
-const secret = process.env.JWT_SECRET;
+import AppError from "../appError.js";
+import { config } from "../config/index.js";
+import { HTTP_UNAUTHORIZED } from "../constants/httpStatus.js";
 
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies.token;
 
   if (!token) {
-    throw new Error("Token is missing!");
+    throw new AppError("Token is missing!", HTTP_UNAUTHORIZED);
   }
 
   try {
-    const decoded = jwt.verify(token, secret as string);
+    const decoded = jwt.verify(token, config.jwtSecret as string);
     req.user = decoded as TokenPayload;
   } catch (error) {
-    throw new Error("Invalid Token");
+    throw new AppError("Invalid Token", HTTP_UNAUTHORIZED);
   }
 
   next();
